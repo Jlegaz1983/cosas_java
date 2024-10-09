b6c9ba93173391a5f3e6b93498cc378792a413b3
jlegaz1983 <jlegaz83@gmail.com>
2024-10-09 16:46:33 +0200

    Primer commit de prueba

---
+++ appMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package saludo_docker;
+
+/**
+ * 
+ */
+public class appMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Estas dentro de docker");
+		
+
+	}
+
+}
